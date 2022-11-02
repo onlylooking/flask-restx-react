06d2a20cfb0bd3644f5d9b120e8b54dea2e81825
luka <lopicicluka@gmail.com>
2022-11-02 20:54:53 +0100

    removed react app for private

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,16 +1,14 @@
-
+import os, json
 from flask import Flask, send_from_directory, request, Blueprint
 from flask_cors import CORS
 
 from .models.database import db
-from private import admin
-from public import core
 from .routes import blueprint as api
 
-app = Flask(__name__, static_url_path='/static/build/static', static_folder='../static/build')
+app = Flask(__name__, static_url_path='/react/build/static', static_folder='../react/build')
 app.config.from_object('api.config.BaseConfig')
 db.init_app(app)
-
+CORS(app)
 
 
 # Setup database
@@ -19,11 +17,45 @@ def initialize_database():
     db.create_all()
 
 
-
-app.register_blueprint(core, url_prefix='/')
-app.register_blueprint(admin, url_prefix='/admin')
+# VIES AND API
 app.register_blueprint(api, url_prefix='/api')
-CORS(app)
+
+# React
+@app.route("/")
+def index():
+    """serves React App"""
+    return send_from_directory(app.static_folder, "index.html")
 
 
+# Static Files
+@app.route("/<path:path>")
+def static_proxy(path):
+    """static folder serve"""
+    file_name = path.split("/")[-1]
+    dir_name = os.path.join(app.static_folder, "/".join(path.split("/")[:-1]))
+    return send_from_directory(dir_name, file_name)
 
+
+# 404
+@app.errorhandler(404)
+def handle_404(e):
+    if request.path.startswith("/api/"):
+        return json.dumps("Endpoint not found"), 404
+    return send_from_directory(app.static_folder, "index.html")
+
+
+@app.after_request
+def after_request(response):
+    """
+       Sends back a custom error with {"success", "msg"} format
+    """
+
+    if int(response.status_code) >= 400:
+        print(response.get_data())
+        response_data = json.loads(response.get_data())
+        if "errors" in response_data:
+            response_data = {"success": False,
+                             "msg": list(response_data["errors"].items())[0][1]}
+            response.set_data(json.dumps(response_data))
+        response.headers.add('Content-Type', 'application/json')
+    return response
